801af21da13ea42a53017c49c1281b9ee12e2473
James Tanner <james.f.tanner@maine.edu>
2013-02-25 12:33:25 -0500

    Initial project

---
+++ FindGoal/src/FindGoal.java
@@ -0,0 +1,21 @@
+/**
+ * James Tanner
+ * COS399: Programming Autonomous Robots
+ * 
+ * Find the Goal MKI
+ */
+
+/**
+ * @author James Tanner
+ *
+ */
+public class FindGoal {
+
+	/**
+	 * 
+	 */
+	public FindGoal() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
